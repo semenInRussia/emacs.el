8942ce77ba31b75fe600c7f11d1fede97d78b7d0
semenInRussia <hrams205@gmail.com>
2021-11-14 21:23:34 +0300

    FEATURE add support of Yasnippet + Company Mode

--- init.el
+++ init.el
@@ -63,6 +63,17 @@
     :config
     (global-company-mode))
 
+(defvar company-mode/enable-yas t
+  "Enable yasnippet for all backends.")
+
+(defun company-mode/backend-with-yas (backend)
+    (if (or (not company-mode/enable-yas) (and (listp backend) (member 'company-yasnippet backend)))
+        backend
+        (append (if (consp backend) backend (list backend))
+                '(:with company-yasnippet))))
+
+(setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
+
 (require 'xah-fly-keys)
 
 (xah-fly-keys-set-layout "qwerty")
