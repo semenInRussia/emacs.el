77d4f750e7e56cb526ac1ed010592e5540ed5200
semenInRussia <hrams205@gmail.com>
2022-09-16 21:53:18 +0300

    Add units of the information for the `calc`

---
+++ .emacs.d/calc.el
@@ -0,0 +1,10 @@
+
+;;; Mode settings stored by Calc on Wed Sep 14 23:27:41 2022
+(setq calc-simplify-mode 'units)
+;;; End of mode settings
+
+
+;;; Custom units stored by Calc on Fri Sep 16 18:58:43 2022
+(setq math-additional-units '((b "0.125 * B" "Bite") (B nil "Byte")))
+;;; End of custom units
+
