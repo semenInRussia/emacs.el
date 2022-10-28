59f3093fa89645e9fd3993a3b580a1f05e56c2ca
semenInRussia <hrams205@gmail.com>
2022-10-28 15:19:54 +0300

    Explore `calendar` (built-in calendar)

---
+++ .emacs.d/lisp/misc/my-calendar.el
@@ -0,0 +1,40 @@
+;;; my-calendar.el --- My configuration of `calendar' -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022 semenInRussia
+
+;; Author: semenInRussia <hrams205@gmail.com>
+;; Version: 0.1
+;; Homepage: https://github.com/semeninrussia/emacs.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; My configuration of `calendar'.
+
+;;; Code:
+
+(leaf calendar
+  :ensure t
+  :bind (:calendar-mode-map
+         ("l" . 'calendar-forward-day)
+         ("j" . 'calendar-backward-day)
+         ("i" . 'calendar-backward-week)
+         ("k" . 'calendar-forward-week)
+         ("]" . 'calendar-forward-year)
+         ("[" . 'calendar-backward-year)
+         ("=" . 'calendar-count-days-region)))
+
+(provide 'my-calendar)
+;;; my-calendar.el ends here
