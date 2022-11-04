1b2c10a2cdfd0822e499922bfd5a80749ad8829f
semenInRussia <hrams205@gmail.com>
2022-11-04 20:19:13 +0300

    [explore] explore `emojify`: emojis in the Emacs buffer

---
+++ .emacs.d/lisp/ui/my-emojify.el
@@ -0,0 +1,31 @@
+;;; my-emojify.el --- My configuration of `emojify' -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022 semenInRussia
+
+;; Author: semenInRussia <hrams205@gmail.com>
+;; Version: 0.1
+;; Homepage: https://github.com/semeninrussia/emacs.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; My configuration of `emojify'.
+
+;;; Code:
+
+(leaf emojify :ensure t :global-minor-mode global-emojify-mode)
+
+(provide 'my-emojify)
+;;; my-emojify.el ends here
