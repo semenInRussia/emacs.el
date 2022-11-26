1b7d7d96fb4417dbc7cdf95665d858f2167f25c2
semenInRussia <hrams205@gmail.com>
2022-11-26 14:43:03 +0300

    [feature] view files of the project with relative way

--- .emacs.d/lisp/env/my-projectile.el
+++ .emacs.d/lisp/env/my-projectile.el
@@ -224,51 +224,95 @@ would be `find-file-other-window' or `find-file-other-frame'"
           (funcall ff (expand-file-name file project-root))
           (run-hooks 'projectile-find-file-hook))))
 
-    (defvar helm-source-projectile-files-list
-      (helm-build-sync-source "Projectile files 2"
-        :before-init-hook                 ;nofmt
-        (lambda ()
-          (add-hook 'helm-after-update-hook #'helm-projectile--move-to-real)
-          (add-hook 'helm-cleanup-hook #'helm-projectile--remove-move-to-real))
-        :candidates                       ;nofmt
-        (lambda ()
-          (when (projectile-project-p)
-            (with-helm-current-buffer
-              (let ((root (f-full (projectile-project-root))))
-                (--map
-                 (cons (s-chop-prefix root (f-full it)) it)
-                 (projectile-project-files root))))))
-        :filtered-candidate-transformer   ;nofmt
-        (lambda (files _source)
-          (with-helm-current-buffer
-            (let* ((root (projectile-project-root))
-                   (file-at-root
-                    (file-relative-name
-                     (expand-file-name helm-pattern root))))
-              (if (or
-                   (string-empty-p helm-pattern)
-                   (assoc helm-pattern files))
-                  files
-                (if (equal helm-pattern file-at-root)
-                    (cl-acons
-                     (helm-ff-prefix-filename helm-pattern nil t)
-                     (expand-file-name helm-pattern)
-                     files)
-                  (cl-pairlis
-                   (list
-                    (helm-ff-prefix-filename helm-pattern nil t)
-                    (helm-ff-prefix-filename file-at-root nil t))
-                   (list
-                    (expand-file-name helm-pattern)
-                    (expand-file-name helm-pattern root))
-                   files))))))
-        :fuzzy-match helm-projectile-fuzzy-match
-        :keymap helm-projectile-find-file-map
-        :help-message 'helm-ff-help-message
-        :mode-line helm-read-file-name-mode-line-string
-        :action helm-projectile-file-actions
-        :persistent-action #'helm-projectile-file-persistent-action
-        :persistent-help "Preview file"))))
+    (defun helm-projectile-file-list-filtered-candidate-transformer (files
+                                                                     ;; nofmt
+                                                                     _source)
+      "File Candiadate Transformer for `helm-source-projectile-files-list'.
+
+Change _SOURCE with FILES of the current project"
+      (lambda ()
+        (with-helm-current-buffer
+          (let* ((root (projectile-project-root))
+                 (file-at-root
+                  (file-relative-name
+                   (expand-file-name helm-pattern root))))
+            (if (or
+                 (string-empty-p helm-pattern)
+                 (assoc helm-pattern files))
+                files
+              (if (equal helm-pattern file-at-root)
+                  (cl-acons
+                   (helm-ff-prefix-filename helm-pattern nil t)
+                   (expand-file-name helm-pattern)
+                   files)
+                (cl-pairlis
+                 (list
+                  (helm-ff-prefix-filename helm-pattern nil t)
+                  (helm-ff-prefix-filename file-at-root nil t))
+                 (list
+                  (expand-file-name helm-pattern)
+                  (expand-file-name helm-pattern root))
+                 files)))))))
+
+    (defun helm-source-projectile-files-list-before-init ()
+      "Function which will be runned before `helm-projectile-files-list'."
+      (add-hook 'helm-after-update-hook #'helm-projectile--move-to-real)
+      (add-hook 'helm-cleanup-hook #'helm-projectile--remove-move-to-real))
+
+    (defvar helm-source-projectile-files-list-before-init-hook nil
+      "Hook which will be runned befor `helm-projectile-files-list'.")
+
+    (add-hook 'helm-source-projectile-files-list-before-init-hook
+              'helm-source-projectile-files-list-before-init)
+
+    (setq helm-source-projectile-files-list
+          (helm-build-sync-source "Projectile files"
+            :before-init-hook 'helm-source-projectile-files-list-before-init-hook
+            :candidates (lambda
+                          ()
+                          (when (projectile-project-p)
+                            (with-helm-current-buffer
+                              (cl-loop with root =
+                                       (projectile-project-root)
+                                       for display in
+                                       (projectile-current-project-files)
+                                       collect
+                                       (cons
+                                        (s-chop-prefix
+                                         (f-full root)
+                                         (f-full display))
+                                        (expand-file-name display root))))))
+            :filtered-candidate-transformer (lambda
+                                              (files _source)
+                                              (with-helm-current-buffer
+                                                (let* ((root (projectile-project-root))
+                                                       (file-at-root
+                                                        (file-relative-name
+                                                         (expand-file-name helm-pattern root))))
+                                                  (if (or
+                                                       (string-empty-p helm-pattern)
+                                                       (assoc helm-pattern files))
+                                                      files
+                                                    (if (equal helm-pattern file-at-root)
+                                                        (cl-acons
+                                                         (helm-ff-prefix-filename helm-pattern nil t)
+                                                         (expand-file-name helm-pattern)
+                                                         files)
+                                                      (cl-pairlis
+                                                       (list
+                                                        (helm-ff-prefix-filename helm-pattern nil t)
+                                                        (helm-ff-prefix-filename file-at-root nil t))
+                                                       (list
+                                                        (expand-file-name helm-pattern)
+                                                        (expand-file-name helm-pattern root))
+                                                       files))))))
+            :fuzzy-match helm-projectile-fuzzy-match
+            :keymap helm-projectile-find-file-map
+            :help-message 'helm-ff-help-message
+            :mode-line helm-read-file-name-mode-line-string
+            :action helm-projectile-file-actions
+            :persistent-action #'helm-projectile-file-persistent-action
+            :persistent-help "Preview file"))))
 
 (defalias 'projectile-project-files 'my-projectile-project-files)
 (defalias 'projectile-root-local 'my-projectile-root-local)
