e837877ade3ff7016139dd3db10ed869b0a322d0
semenInRussia <hrams205@gmail.com>
2022-12-30 13:57:08 +0300

    [replacement] change number of saved recent files to 20000

--- .emacs.d/lisp/misc/my-recentf.el
+++ .emacs.d/lisp/misc/my-recentf.el
@@ -28,7 +28,7 @@
 ;;; Code:
 (leaf recentf
   :global-minor-mode recentf-mode
-  :custom (recentf-max-saved-items . 200)
+  :custom (recentf-max-saved-items . 20000)
   :bind (:xah-fly-command-map
          :package xah-fly-keys
          ("SPC k f" . recentf-open-files))
