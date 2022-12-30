52dc93198993b316d0ccae12302096426ec18c92
semenInRussia <hrams205@gmail.com>
2022-12-30 13:53:08 +0300

    [feature] bind keybindings for `outline` movement commands

--- .emacs.d/lisp/misc/my-outline.el
+++ .emacs.d/lisp/misc/my-outline.el
@@ -25,15 +25,26 @@
 
 ;;; Code:
 
+(defvar my-outline-navigation-map (make-sparse-keymap))
+
 (leaf outline
   :ensure t
-  :bind (:xah-fly-command-map
-         :package xah-fly-keys
-         ("SPC o o" . 'outline-show-entry)
-         ("SPC o d" . 'outline-hide-entry)
-         ("SPC o a" . 'outline-show-all)
-         ("SPC o s" . 'my-outline-cycle)
-         ("SPC o x" . 'my-outline-cycle-buffer))
+  :bind ((:xah-fly-command-map
+          :package xah-fly-keys
+          ("SPC o o" . 'outline-show-entry)
+          ("SPC o d" . 'outline-hide-entry)
+          ("SPC o a" . 'outline-show-all)
+          ("SPC o s" . 'my-outline-cycle)
+          ("SPC o x" . 'my-outline-cycle-buffer)
+          ("SPC o k" . 'my-outline-forward-same-level)
+          ("SPC o i" . 'my-outline-backward-same-level)
+          ("SPC o l" . 'my-outline-next-heading)
+          ("SPC o j" . 'my-outline-previous-heading))
+         (:my-outline-navigation-map
+          ("l" . 'my-outline-next-heading)
+          ("k" . 'my-outline-forward-same-level)
+          ("j" . 'my-outline-previous-heading)
+          ("i" . 'my-outline-backward-same-level)))
   :config                               ;nofmt
   (defun my-outline-cycle ()
     "My version of the `outline-cycle', different is that repeat at last key."
@@ -45,7 +56,37 @@
     "Version of the `outline-cycle-buffer', different is repeat at last key."
     (interactive)
     (outline-cycle-buffer)
-    (repeat-at-last-keystroke)))
+    (repeat-at-last-keystroke))
+
+  (defun my-outline-forward-same-level ()
+    "Go to the forward `outline' heading with same level as at point."
+    (interactive)
+    (outline-forward-same-level 1)
+    (set-transient-map my-outline-navigation-map))
+
+  (defun my-outline-backward-same-level ()
+    "Go to the backward `outline' heading with same level as at point."
+    (interactive)
+    (outline-backward-same-level 1)
+    (set-transient-map my-outline-navigation-map))
+
+  (defun my-outline-backward-same-level ()
+    "Go to the backward `outline' heading with same level as at point."
+    (interactive)
+    (outline-backward-same-level 1)
+    (set-transient-map my-outline-navigation-map))
+
+  (defun my-outline-next-heading ()
+    "Go to the next `outline' heading."
+    (interactive)
+    (outline-next-heading)
+    (set-transient-map my-outline-navigation-map))
+
+  (defun my-outline-previous-heading ()
+    "Go to the previous `outline' heading."
+    (interactive)
+    (outline-previous-heading)
+    (set-transient-map my-outline-navigation-map)))
 
 (provide 'my-outline)
 ;;; my-outline.el ends here
