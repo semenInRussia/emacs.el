6cd3060d669d1f4168717b4edba578c9881d93f1
semenInRussia <hrams205@gmail.com>
2023-01-11 22:40:25 +0300

    [explore] `annotate.el`

---
+++ .emacs.d/lisp/misc/my-annotate.el
@@ -0,0 +1,40 @@
+;;; my-annotate.el --- My configuration of `annotate' -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022 semenInRussia
+
+;; Author: semenInRussia <hrams205@gmail.com>
+;; Version: 0.1
+;; Homepage: https://github.com/semeninrussia/emacs.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; My configuration of `annotate'.
+
+;;; Code:
+
+(leaf annotate
+  :ensure t
+  :hook org-mode-hook
+  :custom ((annotate-use-echo-area . t)
+           (annotate-print-annotation-under-cursor . t)
+           (annotate-print-annotation-under-cursor-prefix . "[ann] "))
+  :bind (:my-org-local-map
+         :package my-org
+         ("n n" . 'annotate-annotate)
+         ("n d" . 'annotate-delete-annotation)))
+
+(provide 'my-annotate)
+;;; my-annotate.el ends here
