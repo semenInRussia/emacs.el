411ccf1c57c4e21fd361440950b5998beeace17b
semenInRussia <hrams205@gmail.com>
2023-01-13 20:03:21 +0300

    add some functions for `rust-mode`
    the following commands was added
    
    - `my-rust-toggle-pub`
    - `my-rust-visit-Cargo`
    - `rust-toggle-mutability`

--- .emacs.d/lisp/languages/my-rust.el
+++ .emacs.d/lisp/languages/my-rust.el
@@ -28,8 +28,45 @@
 ;;; Code:
 (leaf rust-mode
   :ensure t
-  :hook (rust-mode-hook . eglot)
-  :config (add-to-list 'eglot-server-programs '(rust-mode . "rust-analyzer")))
+  :hook (rust-mode-hook . eglot-ensure)
+  :major-mode-map rust
+  :bind (:my-rust-local-map
+         ("p" . 'my-rust-toggle-pub)
+         ("t" . 'my-rust-visit-Cargo.toml)
+         ("m" . 'rust-toggle-mutability))
+  :config (add-to-list 'eglot-server-programs
+                       '(rust-mode "rustup" "run" "stable" "rust-analyzer"))
+
+  (defcustom my-rust-maybe-pub-words
+    '(fn mod struct enum type)
+    "List of the symbols indicating words which can be public in Rust."
+    :type '(repeat symbol)
+    :group 'my)
+
+  (defun my-rust-visit-Cargo.toml ()
+    "Visit Cargo.toml file of current rust crate."
+    (interactive)
+    (find-file (my-rust-find-Cargo.toml-in-directory)))
+
+  (defun my-rust-find-Cargo.toml-in-directory (&optional dir)
+    "Find closest Cargo.toml in the DIR and return path to it."
+    (interactive)
+    (setq dir (or dir default-directory))
+    (let ((cargo.toml (f-join dir "Cargo.toml")))
+      (if (f-exists-p cargo.toml)
+          cargo.toml
+        (my-rust-find-Cargo.toml-in-directory (f-parent dir)))))
+
+  (leaf embrace
+    :after embrace
+    :hook (rust-mode-hook . my-rust-embrace-hook)
+    :config (defun my-rust-embrace-hook
+                ()
+              "Add parens to `embrace' parens for `rust-mode'."
+              (embrace-add-pair ?v "Vec<" ">")
+              (embrace-add-pair ?b "Box<" ">")
+              (embrace-add-pair ?o "Option<" ">")
+              (embrace-add-pair ?r "Result<" ">"))))
 
 (provide 'my-rust)
 ;;; my-rust.el ends here
