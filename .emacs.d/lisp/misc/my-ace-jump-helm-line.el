a06d06f9be98cbddfca9e1c95289abd0f3129c30
semenInRussia <hrams205@gmail.com>
2023-02-01 19:11:30 +0300

    [explore] `ace-jump-helm-line` (bind to `C-l`)

---
+++ .emacs.d/lisp/misc/my-ace-jump-helm-line.el
@@ -0,0 +1,32 @@
+;;; my-ace-jump-helm-line.el --- My configuration of `ace-jump-helm-line' -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022 semenInRussia
+
+;; Author: semenInRussia <hrams205@gmail.com>
+;; Version: 0.1
+;; Homepage: https://github.com/semeninrussia/emacs.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; My configuration of `ace-jump-helm-line'.
+
+;;; Code:
+
+(leaf ace-jump-helm-line :ensure t
+  :bind (:helm-map :package helm ("C-l" . 'ace-jump-helm-line)))
+
+(provide 'my-ace-jump-helm-line)
+;;; my-ace-jump-helm-line.el ends here
