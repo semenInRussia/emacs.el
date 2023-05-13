cdddce9b9c107af5f22fde5ec8f5d04b1a30e0c1
semenInRussia <hrams205@gmail.com>
2023-05-13 22:39:08 +0300

    make `auto-compile` async

--- .emacs.d/lisp/misc/my-auto-compile.el
+++ .emacs.d/lisp/misc/my-auto-compile.el
@@ -25,7 +25,32 @@
 
 ;;; Code:
 
-(leaf auto-compile :ensure t :global-minor-mode auto-compile-on-save-mode)
+(require 'async)
+(require 'async-bytecomp)
+(require 'f)
+
+(defun my-auto-recompile-current-elisp-file ()
+  "If the opened buffer is an Elisp file and it was be compiled, recompile it."
+  (and
+   (buffer-file-name)
+   (eq major-mode 'emacs-lisp-mode)
+   (f-exists-p (f-swap-ext (buffer-file-name) "elc"))
+   (async-byte-compile-file (buffer-file-name))))
+
+(define-minor-mode my-auto-recompile-current-elisp-file-mode
+  "After each save of an Elisp file that was be compiled, recompile it.
+
+If the ARG is non-nil, then enable the mode, otherwise disable it."
+  :init-value t
+  (if my-auto-recompile-current-elisp-file-mode
+      (add-hook 'after-save-hook 'my-auto-recompile-current-elisp-file t)
+    (remove-hook 'after-save-hook 'my-auto-recompile-current-elisp-file t)))
+
+(define-global-minor-mode my-global-auto-recompile-current-elisp-file-mode
+  my-auto-recompile-current-elisp-file-mode
+  (lambda () (my-auto-recompile-current-elisp-file-mode t)))
+
+(my-global-auto-recompile-current-elisp-file-mode t)
 
 (provide 'my-auto-compile)
 ;;; my-auto-compile.el ends here
