3782fa4f3267380d6386300e8e671e65840197cb
semenInRussia <hrams205@gmail.com>
2023-05-13 22:16:24 +0300

    support of c++

---
+++ .emacs.d/lisp/languages/my-c.el
@@ -0,0 +1,42 @@
+;;; my-c.el --- My configuration of c and c++ languages -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 semenInRussia
+
+;; Author: semenInRussia <hrams205@gmail.com>
+;; Version: 0.1
+;; Homepage: https://github.com/semeninrussia/emacs.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; My configuration of c and c++ languages
+
+;;; Code:
+
+(leaf cc-mode
+  :ensure t
+  :config                               ;nofmt
+
+  (leaf flycheck-google-cpplint
+    :ensure t
+    :require t
+    :config (flycheck-add-next-checker 'c/c++-gcc '(info . c/c++-googlelint)))
+
+  (leaf google-c-style
+    :ensure t
+    :hook (c++-mode-hook . google-set-c-style)))
+
+(provide 'my-c)
+;;; my-c.el ends here
