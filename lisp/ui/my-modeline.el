a737b1cfe2a93cf7d778db5e39d73a7db43b8f21
semenInRussia <hrams205@gmail.com>
2023-07-07 19:42:12 +0300

    [feat] add `my-modeline-at-top-mode`

--- lisp/ui/my-modeline.el
+++ lisp/ui/my-modeline.el
@@ -23,17 +23,20 @@
 
 ;;; Commentary:
 
-;; My configuration for `modeline'
+;; My configuration for modeline
 
 ;;; Code:
 
 (require 'my-leaf)
 
 
+(leaf nerd-icons :ensure t)
+
 (leaf doom-modeline
   :ensure t
+  :ensure t
   :custom (;; it looks like more nice
-           (doom-modeline-height . 60)
+           (doom-modeline-height . 45)
            ;; enconding not useful I think.
            (doom-modeline-buffer-encoding . nil)
            ;; don't show directory names in `doom-modeline'
@@ -44,7 +47,7 @@
   ;; I use Emacs in fullscreen mode, so I don't see time that provided
   ;; by OS, so I need time in modeline.  EMACS IS MY OS!!!
   ;; I need only to time (not date) in 24hour format
-  (defvar display-time-format)  ;; make compile happy
+  (defvar display-time-format) ;; make compile happy
   (setq display-time-format "%H:%M")
   (display-time-mode 1)
 
@@ -52,6 +55,17 @@
   ;; take off extra place
   (column-number-mode 0)
   (line-number-mode 0))
+
+(define-minor-mode my-modeline-at-top-mode
+  "Place mode-line at the top of the screen."
+  :value nil
+  (if my-modeline-at-top-mode
+      (progn
+        (setq-default header-line-format mode-line-format)
+        (setq-default mode-line-format nil))
+    (setq-default mode-line-format header-line-format)
+    (setq header-line-format nil)))
+
 
 (provide 'my-modeline)
 ;;; my-modeline.el ends here
