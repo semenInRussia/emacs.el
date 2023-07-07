708c611e4f26cdb782a28a0b81b56ee4de113c06
semenInRussia <hrams205@gmail.com>
2023-07-07 19:48:41 +0300

    [feat] add beautiful layout inspired by `nano-emacs`

---
+++ lisp/ui/my-layout.el
@@ -0,0 +1,89 @@
+;;; my-layout.el --- My settings to layout: paddings -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 semenInRussia
+
+;; Author: semenInRussia <hrams205@gmail.com>
+;; Version: 0.1
+;; Homepage: https://github.com/semeninrussia/emacs.el
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; My settings to layout: paddings
+
+;;; Code:
+
+(require 'disp-table)
+
+(setq default-frame-alist
+      (append (list
+               '(min-height . 1)
+               '(height     . 45)
+               '(min-width  . 1)
+               '(width      . 81)
+               '(vertical-scroll-bars . nil)
+               '(internal-border-width . 24)
+               '(left-fringe    . 1)
+               '(right-fringe   . 1)
+               '(tool-bar-lines . 0)
+               '(menu-bar-lines . 0))))
+
+;; on OSX, type the line below (in terminal) to get a 1 pixel border
+;; defaults write com.apple.universalaccess increaseContrast -bool YES
+
+;; To control anti-aliasing on OSX:
+;; defaults write org.gnu.Emacs AppleFontSmoothing -int 0 (none)
+;; defaults write org.gnu.Emacs AppleFontSmoothing -int 1 (light)
+;; defaults write org.gnu.Emacs AppleFontSmoothing -int 2 (medium)
+;; defaults write org.gnu.Emacs AppleFontSmoothing -int 3 (strong)
+
+;; Fall back font for glyph missing in Roboto
+(defface fallback '((t :family "Fira Code"
+                       :inherit 'nano-face-faded)) "Fallback")
+(set-display-table-slot standard-display-table 'truncation
+                        (make-glyph-code ?… 'fallback))
+(set-display-table-slot standard-display-table 'wrap
+                        (make-glyph-code ?? 'fallback))
+
+;; (set-fontset-font t nil "Fira Code" nil 'append)
+
+;; Fix bug on OSX in term mode & zsh (spurious % after each command)
+(add-hook 'term-mode-hook
+          (lambda () (setq buffer-display-table (make-display-table))))
+
+(setq inhibit-startup-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t
+      initial-scratch-message nil)
+
+(tooltip-mode 0)
+(horizontal-scroll-bar-mode 0)
+(scroll-bar-mode 0)
+(menu-bar-mode 0)
+;; (global-hl-line-mode 0)
+(setq x-underline-at-descent-line t)
+
+;; Vertical window divider
+(setq window-divider-default-right-width 24)
+(setq window-divider-default-places 'right-only)
+(window-divider-mode 1)
+
+;; No ugly button for checkboxes
+(setq widget-image-enable nil)
+
+;; Hide org markup for README
+(setq org-hide-emphasis-markers t)
+
+;;; my-layout.el ends here
