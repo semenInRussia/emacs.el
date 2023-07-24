b1b5612ebaf348e7087a964521db5d15ddadf72c
semenInRussia <hrams205@gmail.com>
2023-07-24 17:15:58 +0300

    upgrade the `dired` config

--- lisp/env/my-dired.el
+++ lisp/env/my-dired.el
@@ -28,64 +28,68 @@
 ;;; Code:
 
 (require 'my-leaf)
-
-(defvar dired-filter-map)
-(defvar my-dired-commands-using-minibuffer)
-
-;; the most heavy functions placed at the `my-dired-commands'
-;; and will be evaluated when really needed
-(declare-function my-dired-save-excursion "my-dired-commands.el")
-
 (require 's)
 (require 'dash)
 (require 'just)
 (require 'f)
 
 
+(defvar dired-filter-map)
+
+;; the most heavy functions placed at the `my-dired-commands'
+;; and will be evaluated when really needed (autoloading)
+(declare-function my-dired-save-excursion "my-dired-commands.el")
+(declare-function embark-open-externally "embark")
+
+
 (leaf dired
-  :hook ((dired-mode-hook . dired-hide-details-mode))
-  :defun ((dired-mark
-           dired-do-rename
-           dired-current-directory
-           dired-jump
-           dired-get-file-for-visit)
-          (org-link-open-from-string . ol))
+  ;; don't show extra info about files like:
+  ;; - owner
+  ;; - group
+  ;; - last modified time
+  :hook (dired-mode-hook . dired-hide-details-mode)
+  :defun dired-get-file-for-visit
   :bind (:dired-mode-map
-         (";" . dired-avy)
+         ;; i'm the user of `meow' with hjkl, where "h" is right, so i press
+         ;; right to go the "back" directory
          ("h" . dired-up-directory)
-         ("A" . agnifize-dwim)
-         ("~" . my-dired-jump-to-home))
+         ("A" . agnifize-dwim))
   :config
+  ;; some my commands for `dired'
+  (leaf my-dired-commands
+    :bind (:dired-mode-map
+           :package dired
+           ("~" . my-dired-jump-to-home)
+           ("C-x h"   . my-dired-mark-all-files)
+           ("C-y"     . my-dired-duplicate)
+           ("C-o"     . my-dired-new-file)))
+
   (leaf dired-async
     :ensure async
     :defun dired-async-mode
     :global-minor-mode dired-async-mode)
 
-  (leaf my-dired-commands
-    :bind (:dired-mode-map
-           :package dired
-           ;; Mark anything
-           ("C-x h"   . my-dired-mark-all-files)
-           ;; Manipulation with file(s)
-           ;; copy/move/paste also defines in the section "Dired Hacks: Ranger"
-           ("C-y"     . my-dired-duplicate)
-           ("C-o"     . my-dired-new-file)))
+  (leaf dired-hacks-utils
+    :ensure t)
 
+  ;; filter files from the buffre
   (leaf dired-filter
-    :ensure t
-    :require t
+    :ensure (dired-filter
+             :repo "Fuco1/dired-hacks"
+             :host github)
     :bind-keymap (:dired-mode-map
                   :package dired
                   ("." . dired-filter-map)))
 
+  ;; open PDF and other not in Emacs
   (leaf dired-open
     :ensure t
     :bind (:dired-mode-map
            :package dired
            ("RET" . 'dired-open-file))
-    :push ((dired-open-functions . 'my-dired-open-function-pdf))
     :defvar dired-open-functions
-    :defun (my-dired . (my-pdf-file my-try-open-pdf-file))
+    :push ((dired-open-functions . 'my-dired-open-function-pdf))
+    :defun (my-dired . (my-pdf-file-p my-try-open-pdf-file))
     :config                             ;nofmt
     (defun my-dired-open-function-pdf ()
       "Open function for `dired-open-functions'."
@@ -93,16 +97,16 @@
 
     (defun my-try-open-pdf-file (filename)
       "If file at FILENAME is a pdf file, then open as pdf, other return nil."
-      (when (my-pdf-file filename)
-        (org-link-open-from-string filename)
+      (when (my-pdf-file-p filename)
+        (embark-open-externally filename)
         t))
 
-    (defun my-pdf-file (filename)
+    (defun my-pdf-file-p (filename)
       "Return t, when FILENAME is path to a PDF file."
       (s-suffix-p ".pdf" filename)))
 
   (leaf dired-subtree
-    :ensure t
+    :ensure (dired-subtree :repo "Fuco1/dired-hacks" :host github)
     :defun dired-subtree-beginning
     :bind (:dired-mode-map
            :package dired
@@ -117,18 +121,24 @@
        (forward-line -1)
        (dired (thing-at-point 'filename)))))
 
+  ;; show directories with 1 file
+  ;;
+  ;; it be like "a/b.txt", instead of just "a"
   (leaf dired-collapse
-    :ensure t
+    :ensure (dired-collapse :repo "Fuco1/dired-hacks" :host github)
     :hook dired-mode-hook)
 
+  ;; icons inside `dired'
   (leaf nerd-icons-dired
-    :ensure t
+    :ensure (nerd-icons-dired :repo "rainstormstudio/nerd-icons-dired" :host github)
     :hook dired-mode-hook)
 
   ;; Command for printing file
-  (with-eval-after-load 'lpr (setq lpr-command "PDFToPrinter"))
+  (with-eval-after-load 'lpr
+    (setq lpr-command "PDFToPrinter"))
 
+  ;; ???
   (remove-hook 'dired-mode-hook 'dired-mode))
 
-(provide 'my-dired)
 ;;; my-dired.el ends here
+(provide 'my-dired)
