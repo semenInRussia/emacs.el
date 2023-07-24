332de458cda3bd3b209467ad8114672cda802741
semenInRussia <hrams205@gmail.com>
2023-07-24 17:11:24 +0300

    when an Emacs server have already started, don’t do it again

--- lisp/my-emacsclient.el
+++ lisp/my-emacsclient.el
@@ -25,7 +25,15 @@
 
 ;;; Code:
 
-(add-hook 'emacs-startup-hook 'server-start)
+(declare-function server-running-p "server")
+
+(add-hook
+ 'emacs-startup-hook
+ (defun my-maybe-server-start ()
+   "Run the Emacs server if servers haven't been started."
+   (require 'server)
+   (unless (server-running-p)
+     (server-start))))
 
 (provide 'my-emacsclient)
 ;;; my-emacsclient.el ends here
