b2d2f0ba88f779127465c599951b1dcca2379b36
semenInRussia <hrams205@gmail.com>
2023-07-28 17:21:15 +0300

    rebind `open-junk-file` (from `C-t` to `C-c t`)

--- lisp/misc/my-open-junk-file.el
+++ lisp/misc/my-open-junk-file.el
@@ -23,12 +23,16 @@
 
 ;;; Commentary:
 
-;; My config for open-junk-file
+;; My config for `open-junk-file'.
 
 ;;; Code:
+
 (require 'my-leaf)
 
-(leaf open-junk-file :ensure t :bind ("C-t" . 'open-junk-file))
+
+(leaf open-junk-file
+  :ensure t
+  :bind ("C-c t" . 'open-junk-file))
 
 (provide 'my-open-junk-file)
 ;;; my-open-junk-file.el ends here
