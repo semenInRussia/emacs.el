88ef056976bc8bede574db9a027438f112b6e1c5
semenInRussia <hrams205@gmail.com>
2023-08-03 12:36:55 +0300

    add `capf` for `yasnippet`(s)

--- lisp/editing/my-yas.el
+++ lisp/editing/my-yas.el
@@ -30,6 +30,7 @@
 
 (require 'my-leaf)
 (require 's)
+(require 'dash)
 
 
 (leaf yasnippet
@@ -42,6 +43,61 @@
                               ',(list
                                  (locate-user-emacs-file "snippets")))
             (yas-wrap-around-region . t)))
+
+(declare-function cape--table-with-properties "cape")
+(declare-function cape--bounds "cape")
+(declare-function cape-interactive "cape")
+
+(with-eval-after-load 'cape
+  (defvar my-yas--capf-properties
+    (list :annotation-function (lambda (_) " Snippet ")
+          :company-kind (lambda (_) 'snippet)
+          :exit-function 'my-yas-capf--exit
+          :company-docsig 'my-yas-capf--docsig
+          :exclusive 'no)
+    "Completion extra properties for `my-yas-capf'.")
+
+  (defun my-yas-capf (&optional interactive)
+    "Completion at point for `yasnippet'.
+
+If INTERACTIVE is true, show the completion where suggested only snippets."
+    (interactive (list t))
+    (if interactive
+        (cape-interactive #'my-yas-capf)
+      (when-let (snippets (yas-active-keys))
+        (let ((bounds (cape--bounds 'symbol)))
+          `(,(car bounds) ,(cdr bounds)
+            ,(cape--table-with-properties snippets :category 'snippet)
+            ,@my-yas--capf-properties)))))
+
+  (defun my-yas-capf--docsig (key)
+    "Snippet content for `corfu' which show it in the echo area.
+
+It takes the KEY (trigger) of snippet, because user type it and `corfu'
+manipulate with it to show helpful things"
+    (->
+     ;; the first snippets table
+     (yas--get-snippet-tables major-mode)
+     (car)
+     ;; fetch snippets with a given key
+     (yas--fetch key)
+     ;; choose the first
+     (car)
+     (cdr)
+     ;; get its content
+     (yas--template-content)))
+
+  (defun my-yas-capf--exit (name status)
+    "Exit from `my-yas-capf'."
+    (and
+     (eq status 'finished)
+     (yas-expand)))
+
+  (add-hook
+   'corfu-mode-hook
+   (defun my-yas-capf-setup ()
+     "Add capf for `yasnippet'."
+     (add-hook 'completion-at-point-functions 'my-yas-capf 30 'local))))
 
 (provide 'my-yas)
 ;;; my-yas.el ends here
