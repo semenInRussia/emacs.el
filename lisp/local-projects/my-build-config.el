dc9d1526ee6cd5a1f7cf6722c0eba3a0ea50641d
semenInRussia <hrams205@gmail.com>
2023-08-04 12:23:59 +0300

    fix the issue from the previous commit
    the hash of previous commit is 1a7c783

--- lisp/local-projects/my-build-config.el
+++ lisp/local-projects/my-build-config.el
@@ -131,7 +131,7 @@ The same to
     (insert "\n(provide 'my-modules)")
     (when (file-exists-p dest)
       (delete-file dest))
-    (write-region (point-min) (point-max) dest)))
+    (write-region (point-min) (point-max) dest :append)))
 
 (provide 'my-build-config)
 ;;; my-build-config.el ends here
