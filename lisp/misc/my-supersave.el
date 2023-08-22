216673408d54533dab4c242f46c2f749c9ae1cac
semenInRussia <hrams205@gmail.com>
2023-08-22 21:33:49 +0300

    fix `cape-dict` slowness

--- lisp/misc/my-supersave.el
+++ lisp/misc/my-supersave.el
@@ -1,38 +1,26 @@
 ;;; my-supersave.el --- My config for `supersave'
 
-;; Copyright (C) 2022 Semen Khramtsov
+;; Copyright (C) 2022-2023 Semen Khramtsov
 
 ;; Author: Semen Khramtsov <hrams205@gmail.com>
 ;; Version: 0.1
 ;; URL: https://github.com/semenInRussia/emacs.el
 
-;; This file is not part of GNU Emacs.
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 ;;; Commentary:
 
-;; My config for `supersave'
+;; My config for `supersave': automatically save the current file
 
 ;;; Code:
+
 (require 'my-leaf)
+
 
 (leaf super-save
   :ensure t
-  :defvar super-save-triggers
   :global-minor-mode super-save-mode
-  :config (add-to-list 'super-save-triggers 'dired-jump))
+  :init
+  (add-hook 'super-save-triggers 'dired-jump)
+  (remove-hook 'super-save-hook-triggers 'focus-out-hook))
 
 (provide 'my-supersave)
 ;;; my-supersave.el ends here
