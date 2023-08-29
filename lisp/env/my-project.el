9e3bbfdbb628dbdb638f0cf999c57eabc573fb20
semenInRussia <hrams205@gmail.com>
2023-08-29 22:49:37 +0300

    make map for project.el more intuitive

--- lisp/env/my-project.el
+++ lisp/env/my-project.el
@@ -9,10 +9,33 @@
 ;;; Code:
 
 (require 'my-leaf)
+(eval-and-compile
+  (require 'my-macros))
 
 
 (leaf project
-  :bind-keymap ("C-c p" . project-prefix-map)
+  :hook (after-init-hook . my-bind--project)
+  :bind (:project-prefix-map
+         ;; at start `project-dired' is bound with C-x p D, but `dired-jump'
+         ;; (open `dired' in the current directory) is bound with C-x C-j, I
+         ;; love the following convention:
+
+         ;; C-x C-f - find a file in the current directory,
+         ;; C-x p f - find a file in the current project
+
+         ;; C-x C-b - switch to a opened buffer (or also bookmark or register,
+         ;; because `consult')
+         ;; C-x p b - switch to a project buffer,
+
+         ;; so I also do it with:
+         ;; - (j) `project-dired' / `dired-jump'
+         ;; - (%) `project-query-replace-regexp' / `vr/query-replace'
+         ;; - (s) `consult-ripgrep' / `consult-ripgrep'
+         ("j" . project-dired)
+         ("s" . consult-ripgrep)
+         ("%" . project-query-replace-regexp))
+  :config (defun my-bind--project ()
+            (global-set-key (kbd "C-c p") project-prefix-map))
   :config
   (remove-from-list! project-switch-commands
                      ;; remove `vc-dir' from the commands which will be shown
