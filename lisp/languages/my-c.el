59ec2c34a95e70d09ccdc15131e43bb245f0c64b
semenInRussia <hrams205@gmail.com>
2023-08-29 22:48:38 +0300

    [c] use Google indent style for C too (not only C++)

--- lisp/languages/my-c.el
+++ lisp/languages/my-c.el
@@ -2,23 +2,6 @@
 
 ;; Copyright (C) 2023 semenInRussia
 
-;; Author: semenInRussia <hrams205@gmail.com>
-;; Version: 0.1
-;; Homepage: https://github.com/semeninrussia/emacs.el
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 ;;; Commentary:
 
 ;; My configuration of c and c++ languages
@@ -34,7 +17,7 @@
                  (c-hungry-delete-key . t))
   :config (leaf google-c-style
             :ensure (google-c-style :repo "google/styleguide" :host github)
-            :hook (c++-mode-hook . google-set-c-style)))
+            :hook ((c++-mode-hook c-mode-hook)   . google-set-c-style)))
 
 (provide 'my-c)
 ;;; my-c.el ends here
