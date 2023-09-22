8d86eaaa3abf91098cdcdbd88a36940f28903935
semenInRussia <hrams205@gmail.com>
2023-09-22 16:17:04 +0300

    remove LSP support for Elixir (because it doesn't work on my machine)

--- lisp/languages/my-elixir.el
+++ lisp/languages/my-elixir.el
@@ -13,14 +13,6 @@
 
 
 (leaf elixir-mode
-  :ensure t
-  :config
-  ;; This is optional. It automatically runs `M-x eglot` for you whenever you are in `elixir-mode`:
-  ;; (add-hook 'elixir-mode-hook 'eglot-ensure)
-
-  ;; (with-eval-after-load 'eglot
-  ;;   (setf (alist-get 'elixir-mode eglot-server-programs)
-  ;;         '("language_server.bat")))
-  )
+  :ensure t)
 
 ;;; my-elixir.el ends here
