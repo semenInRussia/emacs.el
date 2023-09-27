d4d04491de789ec7509fd957020b47d3bf9034ea
semenInRussia <hrams205@gmail.com>
2023-09-27 21:21:54 +0300

    fix some byte-compile remarks

--- lisp/languages/lisps/my-elisp.el
+++ lisp/languages/lisps/my-elisp.el
@@ -1,22 +1,6 @@
 ;;; my-elisp.el --- My configuration of the elisp
 
-;; Copyright (C) 2022 Semen Khramtsov
-
-;; Author: Semen Khramtsov <hrams205@gmail.com>
-;; Version: 0.1
-;; URL: https://github.com/semenInRussia/emacs.el
-
-;; This file is not part of GNU Emacs.
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; Copyright (C) 2022-2023 semenInRussia
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
@@ -29,10 +13,8 @@
 
 (require 'my-leaf)
 
-(require 'my-lisp)
 (require 's)
 (require 'dash)
-
 
 (leaf elisp-mode
   :config
