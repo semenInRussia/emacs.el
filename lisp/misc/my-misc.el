062a6d16e89effc46af5547d09f3f148d8566169
semenInRussia <hrams205@gmail.com>
2023-09-27 17:56:28 +0300

    don't report native compilation warnings and errors

--- lisp/misc/my-misc.el
+++ lisp/misc/my-misc.el
@@ -6,18 +6,6 @@
 ;; Version: 0.1
 ;; URL: https://github.com/semenInRussia/emacs.el
 
-;; This file is not part of GNU Emacs.
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -67,6 +55,10 @@
    gcs-done))
 
 (add-hook 'emacs-startup-hook #'my-display-startup-time)
+
+;;; Don't display native-comp warnings
+(defvar native-comp-async-report-warnings-errors)
+(setq native-comp-async-report-warnings-errors nil)
 
 (provide 'my-misc)
 ;;; my-misc.el ends here
