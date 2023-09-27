6006fcef02c80dd92a339a2c44412e99c361ea30
semenInRussia <hrams205@gmail.com>
2023-09-27 17:57:22 +0300

    try to fix `nerd-icons`

--- lisp/ui/my-fonts.el
+++ lisp/ui/my-fonts.el
@@ -6,21 +6,6 @@
 ;; Version: 0.1
 ;; URL: https://github.com/semenInRussia/emacs.el
 
-;; This file is not part of GNU Emacs.
-
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 ;;; Commentary:
 
 ;; My configuration for fonts
@@ -37,6 +22,9 @@
   :group 'my
   :type 'string)
 
+(setq nerd-icons-font-family my-fonts-main
+      nerd-icons-scale-factor 0.9)
+
 (defcustom my-font-size
   20
   "Name of the main font to display all."
@@ -46,6 +34,7 @@
 (set-face-attribute 'default nil
                     :height (* my-font-size 10)
                     :family my-fonts-main)
+
 
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
