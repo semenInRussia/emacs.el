4275763571c62014816e442b87dafc2b0d2f5a8d
semenInRussia <hrams205@gmail.com>
2023-10-22 14:31:47 +0300

    don't cleanup `recentf` file at start up
    recentf list is a list of recently opened files, to quickly open them after

--- lisp/misc/my-recentf.el
+++ lisp/misc/my-recentf.el
@@ -13,7 +13,14 @@
 
 (leaf recentf
   :global-minor-mode recentf-mode
-  :bind ("C-c r" . recentf))
+  :bind ("C-c r" . recentf)
+  :custom (;; by default, `recentf' load the list of recent file at
+           ;; startup, it can eat our init time.
+           ;;
+           ;; NOTE: that because
+           ;; auto cleanup is disabled, sometimes you must run
+           ;; `recentf-cleanup'
+           (recentf-auto-cleanup . 'never)))
 
 (provide 'my-recentf)
 ;;; my-recentf.el ends here
