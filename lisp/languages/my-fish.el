d3497aaa8a92a222b6f86eb605d5ea3f8527e62e
semenInRussia <hrams205@gmail.com>
2023-12-17 21:32:01 +0300

    add support of `fish`

---
+++ lisp/languages/my-fish.el
@@ -0,0 +1,22 @@
+;;; my-fish.el --- My configuration for fish language: language to configure fish shell -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 semenInRussia
+
+;; Author: semenInRussia <hrams205@gmail.com>
+;; Version: 0.1
+;; Homepage: https://github.com/semeninrussia/emacs.el
+;;; Commentary:
+
+;; My configuration for fish language.
+;;
+;; The Fish Language is a language to configure the fish shell
+
+;;; Code:
+
+(require 'leaf)
+
+
+(leaf fish-mode
+  :ensure t)
+
+;;; my-fish.el ends here
