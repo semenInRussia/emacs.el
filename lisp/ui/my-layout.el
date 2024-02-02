3625d433b991e2a23210f0e5e1f7200ca31dd55d
semenInRussia <hrams205@gmail.com>
2024-02-02 23:48:51 +0300

    clean up; ~my-layout

--- lisp/ui/my-layout.el
+++ lisp/ui/my-layout.el
@@ -25,62 +25,14 @@
 
 ;;; Code:
 
-(require 'disp-table)
-
-;; (setq default-frame-alist
-;;       (append (list
-;;                '(min-height . 1)
-;;                '(height     . 45)
-;;                '(min-width  . 1)
-;;                '(width      . 81)
-;;                '(vertical-scroll-bars . nil)
-;;                '(internal-border-width . 24)
-;;                '(left-fringe    . 1)
-;;                '(right-fringe   . 1)
-;;                '(tool-bar-lines . 0)
-;;                '(menu-bar-lines . 0))))
-
-;; on OSX, type the line below (in terminal) to get a 1 pixel border
-;; defaults write com.apple.universalaccess increaseContrast -bool YES
-
-;; To control anti-aliasing on OSX:
-;; defaults write org.gnu.Emacs AppleFontSmoothing -int 0 (none)
-;; defaults write org.gnu.Emacs AppleFontSmoothing -int 1 (light)
-;; defaults write org.gnu.Emacs AppleFontSmoothing -int 2 (medium)
-;; defaults write org.gnu.Emacs AppleFontSmoothing -int 3 (strong)
-
-;; Fall back font for glyph missing in Roboto
-(defface fallback '((t :family "Fira Code"
-                       :inherit 'nano-face-faded)) "Fallback")
-(set-display-table-slot standard-display-table 'truncation
-                        (make-glyph-code ?… 'fallback))
-(set-display-table-slot standard-display-table 'wrap
-                        (make-glyph-code ?? 'fallback))
-
-;; (set-fontset-font t nil "Fira Code" nil 'append)
-
-;; Fix bug on OSX in term mode & zsh (spurious % after each command)
-(add-hook 'term-mode-hook
-          (lambda () (setq buffer-display-table (make-display-table))))
-
 (setq inhibit-startup-screen t
       inhibit-startup-message t
       inhibit-startup-echo-area-message t
       initial-scratch-message nil)
 
-;; (global-hl-line-mode 0)
-(setq x-underline-at-descent-line t)
+(require 'my-leaf)
 
-;; Vertical window divider
-(setq window-divider-default-right-width 24)
-(setq window-divider-default-places 'right-only)
-(window-divider-mode 1)
-
-;; No ugly button for checkboxes
-(setq widget-image-enable nil)
-
-;; Hide org markup for README
-(setq org-hide-emphasis-markers t)
+(setq frame-title-format '(buffer-file-name "%f" ("%b")))
 
 ;; paddings
 (leaf spacious-padding
