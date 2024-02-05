cd312dbc3fcc3db8b21da7dab42c61c6bdbd50af
semenInRussia <hrams205@gmail.com>
2024-02-05 22:25:04 +0300

    speed up; autoload `recentf`

--- lisp/misc/my-recentf.el
+++ lisp/misc/my-recentf.el
@@ -1,6 +1,6 @@
 ;;; my-recentf.el --- My configuration of `recentf' -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2022-2023 semenInRussia
+;; Copyright (C) 2022-2024 semenInRussia
 
 ;;; Commentary:
 
@@ -12,7 +12,9 @@
 
 
 (leaf recentf
-  :global-minor-mode recentf-mode
+  ;; :global-minor-mode recentf-mode
+  :hook (prog-mode-hook
+         text-mode-hook)
   :bind ("C-c r" . recentf)
   :custom (;; by default, `recentf' load the list of recent file at
            ;; startup, it can eat our init time.
@@ -20,7 +22,14 @@
            ;; NOTE: that because
            ;; auto cleanup is disabled, sometimes you must run
            ;; `recentf-cleanup'
-           (recentf-auto-cleanup . 'never)))
+           (recentf-auto-cleanup . 'never))
+  :config
+  (advice-add
+   'consult-buffer :before
+   (defun my-recentf-load ()
+     (interactive)
+     (unless (boundp 'recentf-list)
+       (recentf-mode)))))
 
 (provide 'my-recentf)
 ;;; my-recentf.el ends here
