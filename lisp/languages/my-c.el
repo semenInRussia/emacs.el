70061694eff84993e57ca5d220119e5fd5e9b4c8
semenInRussia <hrams205@gmail.com>
2024-04-28 13:52:49 +0300

    c++(sport): some functions for sport programming
    Now:
    - insert samples,
    - create samples file

--- lisp/languages/my-c.el
+++ lisp/languages/my-c.el
@@ -4,10 +4,17 @@
 
 ;;; Commentary:
 
-;; My configuration of c and c++ languages
+;; My configuration of c and c++ languages.  NOTE that I am often use
+;; C++ for sport programming, so config for sport programming features
+;; are located here.  (also my `yasnippets' is an important part of my
+;; workflow, for example snippet ! - basic template for start any
+;; problem)
 
 ;;; Code:
+
 (require 'my-leaf)
+(require 'f)
+(require 's)
 
 
 (defvar my-c-backend 'lsp
@@ -35,6 +42,17 @@ Backend is either symbol tags or lsp"
 (my-c-update-backend my-c-backend)
 (add-variable-watcher 'my-c-backend
                       #'my-c-update-backend)
+
+(defvar my-sport-map
+  (define-keymap
+    "C-i" #'my-sport-insert-samples
+    "C-f" #'my-sport-find-samples-file))
+(global-set-key (kbd "C-c ;") my-sport-map)
+
+(defun my-sport-find-samples-file ()
+  "Find input.txt file for current C++ file."
+  (interactive)
+  (find-file "input.txt"))
 
 ;; some settings to compile my C++ file using certain flags,
 ;; optimizations, warnings which are useful for Olympiad programming
@@ -70,8 +88,24 @@ Backend is either symbol tags or lsp"
       (format
        "g++ %s -Wdisabled-optimization -Werror -g"
        (buffer-file-name)))))
-
   (add-to-list 'run-command-recipes 'run-command-sportprog-recipe))
+
+;; In sport programming I sometimes use debugger (gdb) or run
+;; `eshell'.  When I run `gud-gdb' (see `my-realgud') I need to enter
+;; all samples data (located in file input.txt) in one line.  The
+;; following function do it.  I also can call it with (C-c ; C-i, it)
+;; + it is like on (C-x i) which inserts content of the file.
+(defun my-sport-insert-samples ()
+  "And insert the content of the input.txt onto the buffer in one line."
+  (interactive)
+  (and
+   (or (file-exists-p "input.txt")
+       (user-error "File input.txt isn't exists, create it using C-c ; C-f (SPC ; f)"))
+   (->>
+    "input.txt"
+    f-read-text
+    (s-replace "\n" " ")
+    insert)))
 
 (provide 'my-c)
 ;;; my-c.el ends here
