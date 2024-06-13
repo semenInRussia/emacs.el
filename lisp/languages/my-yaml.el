e106dd5f3d9b9d1c7fe558ad6a92be543e26d953
semenInRussia <hrams205@gmail.com>
2024-06-13 17:17:32 +0300

    yaml; introduce `yaml-mode`

---
+++ lisp/languages/my-yaml.el
@@ -0,0 +1,15 @@
+;;; my-yaml.el --- My configuration of yaml -*- lexical-binding: t; -*-
+;; Copyright (C) 2024 semenInRussia
+
+;;; Commentary:
+;; My configuration of yaml: markup language
+
+;;; Code:
+
+(require 'leaf)
+
+(leaf yaml-mode
+  :ensure t)
+
+;;; my-yaml.el ends here
+(provide 'my-yaml)
