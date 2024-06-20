22440447d273a360a0494fefdff52f0ab599017d
semenInRussia <hrams205@gmail.com>
2024-06-20 14:27:20 +0300

    markdown; autoload everything

--- lisp/languages/my-markdown.el
+++ lisp/languages/my-markdown.el
@@ -7,69 +7,17 @@
 ;; My configuration for `markdown-mode'.
 
 ;;; Code:
-
 (require 'my-leaf)
 (require 'just)
 (require 'my-autoformat)
 
-(defcustom my-markdown-imenu-generic-expression
-  '(("title""^\\(.*\\)[\n]=+$" 1)
-    ("h2-" "^\\(.*\\)[\n]-+$" 1)
-    ("h1"   "^# \\(.*\\)$" 1)
-    ("h2"   "^## \\(.*\\)$" 1)
-    ("h3"   "^### \\(.*\\)$" 1)
-    ("h4"   "^#### \\(.*\\)$" 1)
-    ("h5"   "^##### \\(.*\\)$" 1)
-    ("h6"   "^###### \\(.*\\)$" 1)
-    ("fn" "^\\[\\^\\(.*\\)\\]" 1))
-  "List of the specific for `markdown-mode' generic expressions.
-
-See `imenu-generic-expression'"
-  :group 'my
-  :type '(repeat string))
-
-(defun my-markdown-first-letter-of-heading-p ()
-  "Return non-nil, when the cursor placed at the `markdown' heading start."
-  (save-excursion
-    (forward-char -1)
-    (skip-chars-backward " #")
-    (bolp)))
-
-(defun autoformat-markdown-capitalize-heading-line ()
-  "Capitalize first letter of a heading line (lines which started with #)."
-  (and
-   (just-line-prefix-p "#")
-   (my-markdown-first-letter-of-heading-p)
-   (upcase-char -1)))
-
-(defun autoformat-markdown-capitalize-list-item ()
-  "Capitalize first letter of a list item line."
-  (interactive)
-  (and
-   ;; line starts with list item prefix
-   (or
-    (--any-p (just-line-prefix-p it nil 'trim)
-             '("-" "+" "*"))
-    (just-line-regexp-prefix-p
-     " *[0-9]+\\."))
-   ;; the cursor is after the first character
-   (just-call-on-backward-char*
-    (looking-back "^ *\\(-\\|\\+\\|\\*\\|[0-9]+\\.\\) +" nil))
-   ;; then upcase
-   (upcase-char -1)))
-
 (leaf markdown-mode
   :ensure t
-  :hook (markdown-mode-hook .
-                            (lambda ()
-                              (setq-local
-                               imenu-generic-expression
-                               my-markdown-imenu-generic-expression)))
   :config
   (add-hook 'markdown-mode-hook 'visual-line-mode)
 
   (leaf markdown-toc
-    :ensure (markdown-mode :repo "jrblevin/markdown-mode" :host github)
+    :ensure t
     :bind (:markdown-mode-map
            :package markdown-mode
            ("C-T" . markdown-toc-generate-or-refresh-toc)))
@@ -78,12 +26,10 @@ See `imenu-generic-expression'"
     :ensure (edit-indirect :repo "Fanael/edit-indirect" :host github))
 
   (require 'my-autoformat)
-
-  (my-autoformat-bind-for-major-mode
-   'markdown-mode
-   'autoformat-markdown-capitalize-heading-line
-   'autoformat-markdown-capitalize-list-item
-   'my-autoformat-sentence-capitalization))
+  (my-autoformat-bind-for-major-mode 'markdown-mode
+                                     'autoformat-markdown-capitalize-heading-line
+                                     'autoformat-markdown-capitalize-list-item
+                                     'my-autoformat-sentence-capitalization))
 
 (provide 'my-markdown)
 ;;; my-markdown.el ends here
---
+++ lisp/local-projects/my-markdown-funcs.el
@@ -0,0 +1,71 @@
+;;; my-markdown-funcs.el --- Some helper functions for `markdown-mode' -*- lexical-binding: t -*-
+;; semenInRussia 2024
+
+;;; Commentary:
+;; Some helper functions for `markdown-mode'.
+
+;;; Code:
+(require 'dash)
+(require 'just)
+
+;;; Autoformat (including autocapitalize):
+
+;;;###autoload
+(defun my-markdown-first-letter-of-heading-p ()
+  "Return non-nil, when the cursor placed at the `markdown' heading start."
+  (save-excursion
+    (forward-char -1)
+    (skip-chars-backward " #")
+    (bolp)))
+
+;;;###autoload
+(defun autoformat-markdown-capitalize-heading-line ()
+  "Capitalize first letter of a heading line (lines which started with #)."
+  (and
+   (just-line-prefix-p "#")
+   (my-markdown-first-letter-of-heading-p)
+   (upcase-char -1)))
+
+;;;###autoload
+(defun autoformat-markdown-capitalize-list-item ()
+  "Capitalize first letter of a list item line."
+  (interactive)
+  (and
+   ;; line starts with list item prefix
+   (or
+    (--any-p (just-line-prefix-p it nil 'trim)
+             '("-" "+" "*"))
+    (just-line-regexp-prefix-p
+     " *[0-9]+\\."))
+   ;; the cursor is after the first character
+   (just-call-on-backward-char*
+    (looking-back "^ *\\(-\\|\\+\\|\\*\\|[0-9]+\\.\\) +" nil))
+   ;; then upcase
+   (upcase-char -1)))
+
+;;; Imenu:
+
+(defcustom my-markdown-imenu-generic-expression
+  '(("title""^\\(.*\\)[\n]=+$" 1)
+    ("h2-" "^\\(.*\\)[\n]-+$" 1)
+    ("h1"   "^# \\(.*\\)$" 1)
+    ("h2"   "^## \\(.*\\)$" 1)
+    ("h3"   "^### \\(.*\\)$" 1)
+    ("h4"   "^#### \\(.*\\)$" 1)
+    ("h5"   "^##### \\(.*\\)$" 1)
+    ("h6"   "^###### \\(.*\\)$" 1)
+    ("fn" "^\\[\\^\\(.*\\)\\]" 1))
+  "List of the specific for `markdown-mode' generic expressions.
+
+See `imenu-generic-expression'"
+  :group 'my
+  :type '(repeat string))
+
+(when (eq major-mode 'markdown-mode)
+  (setq-local imenu-generic-expression my-markdown-imenu-generic-expression))
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (setq-local imenu-generic-expression my-markdown-imenu-generic-expression)))
+
+(provide 'my-markdown-funcs)
+;;; my-markdown-funcs.el ends here
