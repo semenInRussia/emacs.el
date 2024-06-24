c45dd15c65a390a41dea5cbaae824d5fa70406f2
semenInRussia <hrams205@gmail.com>
2024-06-24 16:33:56 +0300

    try to decrease init

--- lisp/env/my-hl-todo.el
+++ lisp/env/my-hl-todo.el
@@ -12,9 +12,25 @@
 ;;; Code:
 (require 'my-leaf)
 
-(leaf hl-todo
-  :ensure (hl-todo :repo "tarsius/hl-todo" :host github)
-  :global-minor-mode global-hl-todo-mode)
+;; (leaf hl-todo
+;;   :ensure (hl-todo :repo "tarsius/hl-todo" :host github)
+;;   :global-minor-mode global-hl-todo-mode)
+
+(defface my-hl-todo-face  '((t (:bold nil :italic t :foreground "#cc9393")))
+  "A face to highlight things like TODO and NOTE.")
+
+(define-minor-mode my-hl-todo-mode
+  "Highlight things like TODO and NOTE."
+  :init-value t
+  (when (and my-hl-todo-mode
+             (not (eq major-mode 'org-mode)))
+    (font-lock-add-keywords
+     nil
+     '(("\\(TODO\\|NOTE\\|DONE\\|PERF\\|HACK\\)" . 'my-hl-todo-face))
+     t)))
+
+(add-hook 'prog-mode-hook #'my-hl-todo-mode)
+(add-hook 'text-mode-hook #'my-hl-todo-mode)
 
 (provide 'my-hl-todo)
 ;;; my-hl-todo.el ends here
