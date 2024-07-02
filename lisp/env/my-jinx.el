bdbc2be716efd8f057ae6c96b46c9b50f9b4a14a
semenInRussia <hrams205@gmail.com>
2024-07-02 20:54:16 +0300

    jinx; use a bit of idle time to load `jinx` in the buffer

--- lisp/env/my-jinx.el
+++ lisp/env/my-jinx.el
@@ -16,16 +16,32 @@
 
 (defvar my-enchant-executable "enchant" "A path to executable of the Enchant program.")
 
+(defun my-jinx-ensure ()
+  "Load `jinx' for current buffer."
+  (interactive)
+  ;; run it with idle timer.  It's useful, because in this case Emacs
+  ;; don't need to load `jinx' instantly after somebody open a file,
+  ;; so file will be opened more quickly and lsp will be activated
+  ;; after some time
+  (let ((buf (current-buffer)))
+    (run-with-idle-timer 1 nil
+                         (lambda ()
+                           (when (buffer-live-p buf)
+                             (with-current-buffer buf
+                               (jinx-mode)))))))
+
 (leaf jinx
   :ensure t
-  :when (and (executable-find "enchant-2")
+  :when (and (executable-find my-enchant-executable)
              (seq-find #'executable-find '("gcc" "clang" "cc")))
   :custom (jinx-languages . "ru_RU en")
+  :commands jinx-mode
   :bind (("M-$" . jinx-correct)
          (:meow-normal-state-keymap
           :package meow-core
           ("$" . jinx-correct)))
-  :hook text-mode-hook prog-mode-hook)
+  :hook ((text-mode-hook . my-jinx-ensure)
+         (prog-mode-hook . my-jinx-ensure)))
 
 ;;; my-jinx.el ends here
 (provide 'my-jinx)
