a90eab8c7ee43a68a029a6ea341d35fef8e3bb8c
Semen <hrams205@gmail.com>
2025-08-01 23:39:18 +0300

    fix `org-mode`

--- lisp/languages/my-org.el
+++ lisp/languages/my-org.el
@@ -9,9 +9,7 @@
 (require 's)
 (require 'dash)
 
-
 (leaf org
-  ;; :ensure t
   :defun (meow-insert . meow-command)
   :custom (;; `org-refile'
            (org-refile-use-outline-path . 'file)
@@ -29,84 +27,84 @@
           ("C-c C-j"   . org-id-get-create)))
   :config
   (add-hook 'org-mode-hook 'visual-line-mode)
-  (add-hook 'org-mode-hook 'aas-activate-for-major-mode)
+  (add-hook 'org-mode-hook 'aas-activate-for-major-mode))
 
-  (leaf my-org-editing
-    :bind (:org-mode-map
-           :package org
-           ("C-c M-i"   . my-org-insert-image)
-           ("C-c M-u"   . my-org-insert-img-at-url)
-           ("C-c C-t"   . my-org-todo)))
+(leaf my-org-editing
+  :bind (:org-mode-map
+         :package org
+         ("C-c M-i"   . my-org-insert-image)
+         ("C-c M-u"   . my-org-insert-img-at-url)
+         ("C-c C-t"   . my-org-todo)))
 
-  (leaf toc-org :ensure t)
+(leaf toc-org :ensure t)
 
-  (leaf org-preview
-    :ensure (org-preview :repo "karthink/org-preview"
-                         :host github)
-    :commands org-preview-mode)
+(leaf org-preview
+  :ensure (org-preview :repo "karthink/org-preview"
+                       :host github)
+  :commands org-preview-mode)
 
-  (leaf xenops
-    :ensure t
-    :bind (:org-mode-map
-           :package org
-           ("C-c C-x C-l" . xenops-mode))
-    :custom (xenops-math-image-scale-factor . 2))
+(leaf xenops
+  :ensure t
+  :bind (:org-mode-map
+         :package org
+         ("C-c C-x C-l" . xenops-mode))
+  :custom (xenops-math-image-scale-factor . 2))
 
-  (leaf laas
-    :ensure t
-    :hook org-mode-hook)
+(leaf laas
+  :ensure t
+  :hook org-mode-hook)
 
-  ;; format `org-mode' code after every key hit
-  (leaf my-org-autoformat
-    :hook (org-mode-hook . my-autoformat-mode))
+;; format `org-mode' code after every key hit
+(leaf my-org-autoformat
+  :hook (org-mode-hook . my-autoformat-mode))
 
-  (leaf consult
-    :bind (:org-mode-map
-           :package org
-           ([remap consult-imenu] . consult-outline)))
+(leaf consult
+  :bind (:org-mode-map
+         :package org
+         ([remap consult-imenu] . consult-outline)))
 
-  ;; remove some useless things from the current `org-mode' buffer
-  (leaf my-org-do-tidy
-    :bind (:org-mode-map
-           :package org
-           ("C-c M-q" . my-org-tidy)))
+;; remove some useless things from the current `org-mode' buffer
+(leaf my-org-do-tidy
+  :bind (:org-mode-map
+         :package org
+         ("C-c M-q" . my-org-tidy)))
 
-  ;; transient to change values of #+OPTIONS and other #+<THINGS>
-  ;;
-  ;; (Info-goto-node "(org)Export Settings")
-  ;; (leaf my-org-options
-  ;;   :bind (:org-mode-map
-  ;;          :package org
-  ;;          ("C-c C-." . my-org-options-transient)))
+;; transient to change values of #+OPTIONS and other #+<THINGS>
+;;
+;; (Info-goto-node "(org)Export Settings")
+;; (leaf my-org-options
+;;   :bind (:org-mode-map
+;;          :package org
+;;          ("C-c C-." . my-org-options-transient)))
 
-  ;; very beautiful `org'
-  ;;
-  ;; for example, it show [1/3] like a pie progress. :o
-  (leaf org-modern
-    :ensure t
-    :hook org-mode-hook)
+;; very beautiful `org'
+;;
+;; for example, it show [1/3] like a pie progress. :o
+(leaf org-modern
+  :ensure t
+  :hook org-mode-hook)
 
-  (leaf org-autolist
-    :ensure t
-    ;; :hook org-mode-hook
-    :commands org-autolist-mode
-    :config
-    (run-with-idle-timer 3 nil (lambda () (require 'org-autolist)))
-    (add-hook 'org-mode-hook 'org-autolist-mode))
+(leaf org-autolist
+  :ensure t
+  ;; :hook org-mode-hook
+  :commands org-autolist-mode
+  :config
+  (run-with-idle-timer 3 nil (lambda () (require 'org-autolist)))
+  (add-hook 'org-mode-hook 'org-autolist-mode))
 
-  (leaf rorg
-    :ensure (rorg :host github :repo "semenInRussia/rorg")
-    :bind (:org-mode-map
-           :package org
-           ("C-c M-s" . rorg-splice-subtree)
-           ("C-c C-0" . rorg-wrap-region-or-current-heading)
-           ("C-c M-(" . rorg-wrap-region-or-current-heading)
-           ("C-c C-{" . rorg-forward-slurp-subtree)
-           ("C-c C-}" . rorg-backward-barf-subtree)
-           ("C-c {" . rorg-backward-slurp-subtree)
-           ("C-c [" . rorg-forward-barf-subtree)))
+(leaf rorg
+  :ensure (rorg :host github :repo "semenInRussia/rorg")
+  :bind (:org-mode-map
+         :package org
+         ("C-c M-s" . rorg-splice-subtree)
+         ("C-c C-0" . rorg-wrap-region-or-current-heading)
+         ("C-c M-(" . rorg-wrap-region-or-current-heading)
+         ("C-c C-{" . rorg-forward-slurp-subtree)
+         ("C-c C-}" . rorg-backward-barf-subtree)
+         ("C-c {" . rorg-backward-slurp-subtree)
+         ("C-c [" . rorg-forward-barf-subtree)))
 
-  (defun doom-docs-org-mode () (interactive)))
+(defun doom-docs-org-mode () (interactive))
 
 (leaf org-download
   :ensure (org-download :repo "abo-abo/org-download" :host github)
