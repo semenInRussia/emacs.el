d8d1eb2be72cef52eca4c6a6a7963ecc6d6cdf4b
Semen <hrams205@gmail.com>
2025-08-05 17:42:34 +0300

    icons inside of `magit`

--- lisp/env/my-git.el
+++ lisp/env/my-git.el
@@ -1,4 +1,4 @@
-;;; my-git.el --- My config for the Git: the most popular version control
+;;; my-git.el --- My config for the Git: the most popular version control -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2022-2025 semenInRussia
 
@@ -32,7 +32,15 @@
 
   (with-eval-after-load 'project
     (add-to-list 'project-switch-commands '(magit-project-status "Magit") t)
-    (keymap-set project-prefix-map "m" 'magit-project-status)))
+    (keymap-set project-prefix-map "m" 'magit-project-status))
+
+  (with-eval-after-load 'magit
+    ;; Show the status, filename and icon (using the ‘nerd-icons’ package).
+    ;; You have to explicitly install the ‘nerd-icons’ package, else this
+    ;; function errors.
+    (eval-and-compile
+      (require 'magit))
+    (setopt magit-format-file-function #'magit-format-file-nerd-icons)))
 
 (leaf git-timemachine
   :ensure (git-timemachine :repo "pidu/git-timemachine" :host gitlab))
