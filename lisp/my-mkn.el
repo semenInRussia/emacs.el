daa20f89def1d8c8d2e3788f979e1e4c3055f555
Semen <hrams205@gmail.com>
2025-11-26 20:58:16 +0300

    introduce `my-mkn-mode` and start equations

--- lisp/my-mkn.el
+++ lisp/my-mkn.el
@@ -7,7 +7,23 @@
 ;; My configuration for life in mkn.
 
 ;;; Code:
+
 (require 'my-leaf)
+(declare-function laas-org-mathp "lass.el")
+
+(defun my-mkn-equation-block ()
+  "Insert begin block equation."
+  (interactive)
+  (if (laas-org-mathp)
+      (progn
+        (search-forward "\\end{equation}")
+        (set-input-method "russian-computer"))
+    (progn
+      (insert "\\begin{equation}\n")
+      (newline)
+      (insert "\\end{equation}\n")
+      (forward-line -2)
+      (set-input-method nil))))
 
 (defun my-mkn-equation ()
   "Insert an equation into `org-mode' abstract."
@@ -25,7 +41,27 @@
 
 (with-eval-after-load 'org
   (keymap-set org-mode-map "M-U" #'my-mkn-equation)
-  (keymap-set org-mode-map "M-I" #'my-mkn-equation-end))
+  (keymap-set org-mode-map "M-I" #'my-mkn-equation-end)
+  (keymap-set org-mode-map "M-E" #'my-mkn-equation-block))
+
+(define-minor-mode my-mkn-mode
+  "Edit/see conspects I scratch on lectures in `org-mode'."
+  :group 'misc
+  :init-value nil
+  (when my-mkn-mode
+    (xenops-mode t)))
+
+(defun my-maybe-mkn-mode ()
+  "Maybe enable `my-mkn-mode'.
+
+I am enable `my-mkn-mode' for files, that have a susbstring \"mkn\" if file
+path."
+  (and
+   (buffer-file-name)
+   (s-contains-p "mkn" (buffer-file-name))
+   (my-mkn-mode +1)))
+
+(add-hook 'org-mode-hook 'my-maybe-mkn-mode)
 
 (provide 'my-mkn)
 ;;; my-mkn.el ends here
